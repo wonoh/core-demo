6ba5edaa9c67163d56f24369de57ea9bd0946e48
wonoh <wonoh@zuminternet.com>
2020-10-16 14:15:09 +0900

    test: 싱글톤 테스트

---
+++ src/test/java/hello/core/singleton/SingletonTest.java
@@ -0,0 +1,36 @@
+package hello.core.singleton;
+
+import hello.core.AppConfig;
+import hello.core.member.MemberService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SingletonTest {
+
+    @Test
+    @DisplayName("스프링 없는 순수 DI 컨테이너")
+    void pureContainer(){
+        AppConfig appConfig = new AppConfig();
+        // 1. 조회 호출할때마다 객체를 생성하는지
+        MemberService memberService = appConfig.memberService();
+
+        MemberService memberService1 = appConfig.memberService();
+
+        System.out.println("memberService1 = " + memberService);
+        System.out.println("memberService = " + memberService1);
+        assertThat(memberService).isNotSameAs(memberService1);
+    }
+
+    @Test
+    @DisplayName("싱글톤 패턴을 적용한 객체 사용")
+    void singletonServiceTest(){
+        SingletonService singletonService1 = SingletonService.getInstance();
+        SingletonService singletonService2 = SingletonService.getInstance();
+        System.out.println("singletonService1 = " + singletonService1);
+        System.out.println("singletonService2 = " + singletonService2);
+        assertThat(singletonService1).isSameAs(singletonService2);
+    }
+
+}
